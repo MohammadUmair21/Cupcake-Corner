c1a8f3993f7d265aa051df733ec0ec273d02732e
UmairAnsari1 <ansariumairislam21@gmail.com>
2024-04-23 14:04:46 +0530

    Initial Commit

---
+++ Cupcake Corner/Cupcake_CornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_CornerApp.swift
+//  Cupcake Corner
+//
+//  Created by Umair on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_CornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
